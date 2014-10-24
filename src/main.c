2d49e65294b188c55884ae63553b085a6b934adc
srli <sophia.li@students.olin.edu>
2014-10-24 16:32:04 -0400

    files

--- src/main.c
+++ src/main.c
@@ -9,23 +9,23 @@
 
 
 // Set up external interrupts for INT0 for any logical change
-int initButton() {
+/*int initButton() {
     EICRA = _BV(ISC00);
     EIMSK = _BV(INT0);
     return(0);
-}
-
+}*/
+/*
 void buttonScript(int target, int val, uint8_t x, uint8_t y){
     char cSREG = SREG; //Store SREG
     PORTB &= _BV(PB4); //set pin PB4 low (turns off LED)
     SREG = cSREG;
 }
+*/
 
-
-ISR(INT0_vect) {
+/*ISR(INT0_vect) {
     buttonScript(NODE_TARGET_1, PIND & _BV(PD6), 0b01, 0b00); //button presses enabled only on pin PD6
 }
-
+*/
 
 int main (void) {
     DDRB |= 0xFF; // set all PORTB pins for output
@@ -41,9 +41,9 @@ int main (void) {
 
 
 
-    sei(); // enable global interrupts    
+  /*  sei(); // enable global interrupts    
     initButton(); // intitialize button interrupts
-
+*/
     for (;;) {
         // keeps led on forever
     }
